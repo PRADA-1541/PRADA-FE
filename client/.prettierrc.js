5a28d235a7fb323ac962be5159dc9e47b2bba367
김준하 <junhakjh@gmail.com>
2023-03-11 01:20:20 +0900

    update: prettier 설치, 설정

---
+++ client/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: true,
+  semi: true,
+  useTabs: false,
+  arrowParens: 'always',
+  printWidth: 120,
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+};
