9f9cc83030b2b0987d1f115dfb5be18d5f6e3cbf
김준하 <junhakjh@gmail.com>
2023-03-11 01:30:49 +0900

    update: eslint 설정

---
+++ client/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['react'],
+};
