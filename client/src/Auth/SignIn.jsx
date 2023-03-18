fd2dfa131b11ea07cf7be6acd787d2ec328a7628
김준하 <junhakjh@gmail.com>
2023-03-18 17:11:42 +0900

    feat: signin 기본 스타일 구성 및 signup 렌더링 시 kakaoToken 서버로 전달

--- client/src/Auth/SignIn.jsx
+++ client/src/Auth/SignIn.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Auth.scss';
 
+const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+const REDIRECT_URI = process.env.REACT_APP_SIGNIN_REDIRECT;
+const KAKAO_LOGIN_API = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
 const SignIn = () => {
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='bg'>
-      <div>
+      <div className='authBox'>
         <h1>PRADA</h1>
         <form>
-          <input type='text' />
-          <input type='password' />
-          <button>로그인</button>
+          <input type='text' placeholder='아이디' />
+          <input type='password' placeholder='비밀번호' />
+          <button onClick={signIn}>로그인</button>
+          <Link>아이디 | 비밀번호 찾기</Link>
+          <Link to={KAKAO_LOGIN_API}>카카오톡</Link>
         </form>
       </div>
     </div>
--- client/src/Auth/SignUp.jsx
+++ client/src/Auth/SignUp.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { SendKakaoToken } from '../api/authService';
 
 const SignUp = () => {
+  useEffect(() => {
+    const urlParams = new URL(location.href).searchParams;
+    const kakaoToken = urlParams.get('code');
+    SendKakaoToken(kakaoToken);
+  }, []);
   return <></>;
 };
 
