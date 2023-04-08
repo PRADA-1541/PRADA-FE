2ff12191214f19b6d8d48549d12340ca621b0f96
김준하 <junhakjh@gmail.com>
2023-04-08 16:44:30 +0900

    style: text-gray-light 추가

--- client/src/Preview/CocktailPreveiw_bg/SimpleCocktailPreview.jsx
+++ client/src/Preview/CocktailPreveiw_bg/SimpleCocktailPreview.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './SimpleCocktailPreview.scss';
 import { AiFillStar } from 'react-icons/ai';
 import defaultImage from '../../assets/images/defaultImage.png';
-import KeyWord from '../../Material/Keyword/KeyWord';
+import KeyWord from '../../Material/KeywordBox/Keyword/KeyWord';
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 
