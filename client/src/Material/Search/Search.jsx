cd3f9149c211de3f92a99dae12a76874e5b35768
김준하 <junhakjh@gmail.com>
2023-04-30 18:44:32 +0900

    feat: 검색창 구현

---
+++ client/src/Material/Search/Search.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import './Search.scss';
+import PropTypes from 'prop-types';
+import useClickState from '../../hooks/useClickState';
+
+const Search = ({ setSearch, placeholder, setText, list }) => {
+  const [searchWord, setSearchWord] = useState('');
+  const [ref, handleClickOutside] = useClickState(setSearch);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+
+  const handleClick = (item) => {
+    setText(item);
+    setSearch(false);
+  };
+
+  return (
+    <div className='searchContainer' ref={ref}>
+      <input
+        className='searchInput'
+        type='text'
+        placeholder={placeholder}
+        onChange={(e) => setSearchWord(e.target.value)}
+        value={searchWord}
+      />
+      <ul className='searchList'>
+        {list.map((item) => {
+          if (item.includes(searchWord)) {
+            return (
+              <li className='searchedItem' key={item} onClick={() => handleClick(item)}>
+                {item}
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
+};
+
+Search.propTypes = {
+  setSearch: PropTypes.func.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  setText: PropTypes.func.isRequired,
+  list: PropTypes.array.isRequired,
+};
+
+export default Search;
