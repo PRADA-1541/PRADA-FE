9618f630939da9c6bbc0a763b2bb97edc45ec05b
김준하 <junhakjh@gmail.com>
2023-05-14 18:22:24 +0900

    fix: drop img baseUrl

--- client/src/api/recipeService.js
+++ client/src/api/recipeService.js
@@ -17,7 +17,7 @@ export const GetRecipePriorInfo = async (setKeywordsList, setUnitList, setIngred
 export const UploadImg = async (directory, formData) => {
   try {
     const res = await Recipe.uploadImg(directory, formData);
-    return process.env.REACT_APP_IMG_BASE_URL + res.data.url;
+    return res.data.url;
   } catch (error) {
     console.log(error);
     return false;
